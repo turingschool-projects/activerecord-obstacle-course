f9ef284b04bad459f7751e445199bd5fd4eadbbf
William Homer <billyhomeriii@gmail.com>
2019-05-13 12:00:44 -0600

    week1 problem 3

--- spec/models/aroc_week_1_spec.rb
+++ spec/models/aroc_week_1_spec.rb
@@ -52,6 +52,7 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
     # Your solution should not contain the ID of the order anywhere
+    order_id = Order.order("amount DESC").limit(1).pluck(:id).first
     # ------------------------------------------------------------
 
     # Expectation
