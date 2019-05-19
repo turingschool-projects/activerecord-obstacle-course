a716e0ec6e6e71709c3cd23ff59a254add40d9ed
William Homer <billyhomeriii@gmail.com>
2019-05-18 20:59:10 -0600

    complete 26

--- spec/models/aroc_week_5_spec.rb
+++ spec/models/aroc_week_5_spec.rb
@@ -34,6 +34,7 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     # Solution goes here
     # When you find a solution, experiment with adjusting your method chaining
     # Which ones are you able to switch around without relying on Ruby's Enumerable methods?
+    ordered_items_names = Item.joins(:order_items).distinct.order(:name).pluck(:name)
     # ---------------------------------------------------------------
 
     # Expectations
