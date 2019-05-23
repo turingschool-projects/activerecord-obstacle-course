215f6b507678d8ecfc6b84bea0c6ac0518d96318
davisc0801 <davisc1014@gmail.com>
2019-05-23 10:46:19 -0600

    Add exercise 29

--- spec/models/aroc_week_5_spec.rb
+++ spec/models/aroc_week_5_spec.rb
@@ -90,7 +90,7 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     expect(custom_results[2].total_item_count).to eq(24)
   end
 
-  xit '29. returns a table of information for all users orders and item counts' do
+  it '29. returns a table of information for all users orders and item counts' do
     # using a single ActiveRecord call, fetch a joined object that mimics the
     # following table of information:
     # ---------------------------------------
@@ -125,7 +125,10 @@ describe 'ActiveRecord Obstacle Course, Week 5' do
     # how will you turn this into the proper ActiveRecord commands?
 
     # ------------------ ActiveRecord Solution ----------------------
-    data = []
+    data = User.joins(orders: :items)
+                .select("users.name as user_name, orders.id as order_id, orders.amount / count(order_items.id) AS avg_item_cost")
+                .group("users.name, orders.id")
+                .order("user_name desc, avg_item_cost asc")
     # ---------------------------------------------------------------
 
     expect([data[0].user_name,data[0].order_id,data[0].avg_item_cost]).to eq([@user_1.name, @order_1.id, 50])
